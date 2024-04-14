ee0c247988f0e9f9f051efe8f324908efc418796
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-14 22:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete this js code
-function Person(name, age) {
-	return class Person {
+
+// Define the Person class
+class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
@@ -10,10 +11,9 @@ function Person(name, age) {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
   }
 }
-}
 
-function Employee(name, age, jobTitle) {
-	return class Employee extends Person {
+// Define the Employee class, which inherits from Person
+class Employee extends Person {
   constructor(name, age, jobTitle) {
     super(name, age);
     this.jobTitle = jobTitle;
@@ -23,12 +23,6 @@ function Employee(name, age, jobTitle) {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
   }
 }
-}
-// Define the Person class
-
-
-// Define the Employee class, which inherits from Person
-
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
